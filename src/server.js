88a131bdd7e97c0a5a566ebd8bd2be9b59553715
notcis <notcis07@gmail.com>
2025-12-05 10:16:01 +0700

    Update server port configuration to include APP_PORT environment variable

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
+import "dotenv/config";
 import { createServer } from "http";
 import app from "./app.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || process.env.APP_PORT || 3000;
 
 const server = createServer(app);
 server.listen(PORT, () => {
